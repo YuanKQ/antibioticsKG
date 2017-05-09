4a4bb5e375cd097f608b767b8767dae558bb9819
yuan <kq_yuan@outlook.com>
2017-05-09 21:30:31 +0800

    Successfully realize the template inheritance[insert another FreeMarker template file (specified by the path parameter) into your template] in FreeMarker by using "<#include path>". For example, hello.ftl uses two template, head.html and navigationBar.html.
    Successfully use JSP and FTL simultaneously if mvc-dispactcher.xml is written correctly.

--- src/main/java/com/iaso/antibiotic/controller/MainController.java
+++ src/main/java/com/iaso/antibiotic/controller/MainController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -13,19 +18,27 @@ import java.net.URLEncoder;
  */
 @Controller
 public class MainController {
-//    @RequestMapping(value = "/index",method = RequestMethod.GET)
-//    public String index() {
-//        return "index";
-//    }
-//
-//    @RequestMapping(value = "/introduce",method = RequestMethod.GET)
-//    public String introduce() {
-//        return "introduce";
-//    }
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/introduce",method = RequestMethod.GET)
+    public String introduce() {
+        return "introduce";
+    }
+
+    /*两种写法都可以
+    @RequestMapping(value = "/hello")
+    public ModelAndView hello(HttpServletRequest request,HttpServletResponse response) {
+        ModelAndView model = new ModelAndView();
+        model.addObject("name", "张三");
+        return model;
+    }
+    */
 
     @RequestMapping(value = "/hello")
-    public String hello(Model model) throws UnsupportedEncodingException {
-//        String name = URLEncoder.encode(, "utf-8");
+    public String hello(Model model) {
         model.addAttribute("name", "张三");
         return "hello";
     }
